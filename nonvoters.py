98a7f0df875d6c60c83d6cbe3e07fe38e6289f7c
Alberto G <algarc04@gmail.com>
2019-04-28 02:14:53 -0500

    Nonvoters written to excell sheet. Have to fix format

--- nonvoters.py
+++ nonvoters.py
@@ -4,7 +4,6 @@ import xlwt
 from openpyxl import Workbook
 from tempfile import TemporaryFile
 
-
 col_num_vuids = 5
 previous_voters = xlrd.open_workbook('Pct 3,5,10 (list)2019.xlsx',{'strings_to_numbers':True})
 voters = xlrd.open_workbook('Voting History clean.xlsx')
@@ -12,79 +11,52 @@ voters = xlrd.open_workbook('Voting History clean.xlsx')
 book = xlwt.Workbook()
 nonvoter_sheet = book.add_sheet('nonvoters')
 
-
+print("Workbooks loaded")
 sname = previous_voters.sheet_names()
 vsname = voters.sheet_names()
 
 previous_voters_sheet = previous_voters.sheet_by_name(sname[0])
 voters_sheet = voters.sheet_by_name(vsname[0])
-print(previous_voters_sheet.nrows)
-print(voters_sheet.nrows)
 
 vuids = []
 for i in range(voters_sheet.nrows):
     vuids.append(voters_sheet.cell(i,col_num_vuids).value)
-print(previous_voters_sheet.nrows)
 
+print("prev Voters: " , previous_voters_sheet.nrows)
+print("recent Voters: ", voters_sheet.nrows)
+
+print("finding nonvoters...")
 nonvoters = []
+col = 0
+n_nonvoters = 0
+
+#Add column labels to worksheet
+for col in range(previous_voters_sheet.ncols):
+            cell_data = previous_voters_sheet.cell_value(0,col)
+            if(cell_data):
+                print(cell_data)
+                cell_data=''
+                nonvoter_sheet.write(0,col,cell_data)
+
+
+#Compare unique voter id, if recent voter not found add to nonvoters worksheet
 for i in range(previous_voters_sheet.nrows):
     found = False
     for j in range(voters_sheet.nrows):
         if(previous_voters_sheet.cell(i,col_num_vuids-1).value == vuids[j]):
             found = True
+
     if(not found):
-        print(previous_voters_sheet.getRow())
-        #nonvoters.append(previous_voters_sheet.getRow(i))
+        n_nonvoters+=1
+        for col in range(previous_voters_sheet.ncols):
+            cell_data = previous_voters_sheet.cell_value(i,col)
+            if(cell_data):
+                nonvoter_sheet.write(i,col,cell_data)
+                cell_data=''
 
-print(nonvoters[0])
-print(nonvoters[1])
 
-for row,col in enumerate(nonvoters):
-    #print(row[0])
-    print(col)
-    #nonvoter_sheet.write(row[0],nonvoters[row])
-
-print(len(nonvoters))
-
+print("nonvoters found: ", n_nonvoters)
 name = 'nonvoters2019.xls'
 book.save(name)
 book.save(TemporaryFile())
-
-'''
-#riginal File
-df = pandas.read_excel(r'./Pct 3,5,10 (list)2019.xlsx') #old 8692
-df1 = pandas.read_excel(r'./Voting History clean.xlsx') #New 3850
-
-df.columns = df.columns.str.strip().str.lower().str.replace(' ','_').str.replace('(','').str.replace(')','')
-#df1.columns = df.columns.str.strip().str.lower().str.replace(' ','_').str.replace('(','').str.replace(')','')
-
-df = df.query( "(precinct == 10)" )
-df1 = df1.query("(Precinct == 10)" )
-#df1.to_string()
-vuids = df1["VUID"]
-print(vuids[3841])
-
-
-
-#for i in range(vuids.size):
-    #if(df1.lookup([df1.index,vuids[i]])):
-print(df.loc[df['vuid'].isin(vuids)])
-#df.to_string()
-
-#print(df)
-#nonvoters = df.isin({},)
-
-#print(df.columns)
-
-#nonvoters = df - df1
-
-
-#print(df[['vuid','precinct','name','status']])
-
-#df.to_excel("nonvoters.xlsx")
-
-#print(((df["Status"] != False) & (df["Precinct"] == 10)))
-#print(df.query("(Status == 0) and (Precinct == 10)"))
-
-
-'''
+print("file saved as ", name)
